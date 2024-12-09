e5b354558d647571f8f47de11555fe92b6858aaf
vayulqq <sergejmihajlov1337@gmail.com>
2024-12-09 16:55:24 +0300

    Update check_updates.cmd

--- check_updates.cmd
+++ check_updates.cmd
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 chcp 437 > nul
 
 set "CURRENT_VERSION=1.0.1"
-set "GITHUB_URL=https://raw.githubusercontent.com/vayulqq/zapret/refs/heads/main/.service/version.txt"
+set "GITHUB_URL=https://raw.githubusercontent.com/vayulqq/zapret/main/refs/heads/main/.service/version.txt"
 set "RELEASE_URL=https://github.com/vayulqq/zapret/releases"
 set "VERSION_FILE=version.txt"
 set "SKIP_VERSION=null"
