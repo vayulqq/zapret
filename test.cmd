76e927ebbb96e56142ebb7669aaaf90fcaadd582
vayulqq <sergejmihajlov1337@gmail.com>
2024-12-26 13:59:31 +0300

    Create test.cmd

---
+++ test.cmd
@@ -0,0 +1,12 @@
+@echo off
+:: Проверка, запущен ли скрипт от имени администратора
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Запуск от имени администратора...
+    powershell -Command "Start-Process cmd -ArgumentList '/c \"%~dpnx0\" %*' -Verb RunAs"
+    exit /b
+)
+
+:: Ваши команды здесь
+echo Скрипт запущен от имени администратора.
+pause
